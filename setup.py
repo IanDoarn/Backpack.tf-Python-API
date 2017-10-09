1aaff9f6d2737cac227e15dafdddc7efd7c50f83
IanDoarn <ian.doarn@zimmerbiomet.com>
2017-10-08 22:01:06 -0500

    Update README.md

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pybptf',
+    version='1.0.0',
+    packages=['bptf', 'bptf.webcalls'],
+    url='https://github.com/IanDoarn/Backpack.tf-Python-API',
+    license='MIT',
+    author='Ian Doarn',
+    author_email='ibd1997@yahoo.com',
+    description='Python Wrapper for Backpack.tf API'
+)
